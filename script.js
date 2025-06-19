63cf4d57a82e262a6a3e4dd2feb0c349f1325a28
ricardoc-costa <ricardocccorreia@gmail.com>
2025-06-19 07:42:10 -0300

    DEVSORTEIO

---
+++ script.js
@@ -0,0 +1,15 @@
+const sortear = document.querySelector(".sortear")
+const resultado = document.querySelector(".resultado")
+
+function getRandomIntInclusive() {
+const min = document.querySelector(".de").value
+const max = document.querySelector(".ate").value
+
+const arred = Math.floor(Math.random() * (max - min)+1)
+const numero = Number(arred) + Number(min)
+
+resultado.innerHTML = numero
+console.log(numero)
+}
+
+sortear.addEventListener("click", getRandomIntInclusive)
