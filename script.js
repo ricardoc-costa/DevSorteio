3c15bdac95b22ac9d5299453bdd20fa2b7ec868f
ricardoc-costa <ricardocccorreia@gmail.com>
2025-06-20 12:14:09 -0300

    impossibilita de usar intervalos onde o valor máximo é menor que o mínimo

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ function getRandomIntInclusive() {
 const min = document.querySelector(".de").value
 const max = document.querySelector(".ate").value
 
+if(max < min){
+    alert("Ateção, intervalo inválido, pois o valor superior deve ser maior que o valor inferior")
+}else{
 const arred = Math.floor(Math.random() * (max - min + 1))
 const numero = Number(arred) + Number(min)
 
 resultado.innerHTML = numero
 console.log(numero)
 }
-
+}
 sortear.addEventListener("click", getRandomIntInclusive)
