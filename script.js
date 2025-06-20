e28efa32ad0e6622ec479ceea62f318a08099e9c
ricardoc-costa <ricardocccorreia@gmail.com>
2025-06-20 08:58:21 -0300

    correção sorteio

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getRandomIntInclusive() {
 const min = document.querySelector(".de").value
 const max = document.querySelector(".ate").value
 
-const arred = Math.floor(Math.random() * (max - min)+1)
+const arred = Math.floor(Math.random() * (max - min + 1))
 const numero = Number(arred) + Number(min)
 
 resultado.innerHTML = numero
